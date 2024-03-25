c8c3e70b57f8b09c2812e6e823c8d31f8ca7d39e
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-03-25 20:59:45 +0100

    Initial version - coordinates only (#5)
    * Add main entry point + misc cleanup
    * add dependencies to pyproject.toml
    * Add the actual code from Atmospec with minor changes

--- src/harmonwig/__init__.py
+++ src/harmonwig/__init__.py
@@ -3,8 +3,12 @@ Copyright (c) 2024 Daniel Hollas. All rights reserved.
 
 harmonwig: Harmonic Wigner sampling from QM calculations.
 """
+
 from __future__ import annotations
+
+from .harmonwig import HarmonicWigner
 
 __version__ = "0.1.0a0"
 
-__all__ = ["__version__"]
+# TODO: Expose more interface?
+__all__ = ["__version__", "HarmonicWigner"]
